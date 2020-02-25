771a160d24622d1073769c4da6a62b35a4ff0be0
bubba <robertbrassett@cmail.carleton.ca>
2020-02-25 14:23:43 -0500

    Added base implementation of webpage

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,93 @@
 import React from 'react';
-import './App.css';
+import ButtonList from './Components/ButtonList';
 
 class App extends React.Component {
+  state = {
+    options: [
+      {
+        id: 1,
+        type: 'package',
+        identifier: 'A',
+        selected: false
+      },
+      {
+        id: 2,
+        type: 'package',
+        identifier: 'B',
+        selected: false
+      },
+      {
+        id: 3,
+        type: 'delivery',
+        identifier: 'A',
+        selected: false
+      },
+      {
+        id: 4,
+        type: 'delivery',
+        identifier: 'B',
+        selected: false
+      }
+
+    ]
+  };
+
+  selectPackage = (id) => {
+    this.setState({
+      options: this.state.options.map(btn => {
+        if(btn.id === id) {
+          btn.selected = !btn.selected;
+        }
+        return btn;
+      })
+    })
+  }
+
+  selectDelivery = (id) => {
+    this.setState({
+      options: this.state.options.map(btn => {
+        if(btn.id === id) {
+          btn.selected = !btn.selected;
+        }
+        return btn;
+      })
+    })
+  }
+
+  submitRequest = () => {
+    this.state.options.forEach( (btn) => {
+          if (btn.selected) {
+            console.log(btn.selected);
+            btn.selected = false;
+            console.log(btn.selected);
+          }
+        }
+    )
+
+    console.log("Send submit was successful");
+  }
+
   render() {
     return (
         <div className="App">
-          test
+          <h1>Packages:</h1>
+          <ButtonList buttons={this.state.options} selectPackage={this.selectPackage} selectDelivery={this.selectDelivery}/>
+          <div>
+            <h1>Submit</h1>
+            <button style={submitBtnStyle} onClick={this.submitRequest}>Submit</button>
+          </div>
         </div>
+
     );
   }
+}
+
+const submitBtnStyle = {
+  background:'gray',
+  color: 'black',
+  border: 'solid',
+  padding: '20px 20px',
+  cursor: 'pointer',
 }
 
 export default App;
---
+++ app/src/Components/ButtonList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Package from './Package';
+import Delivery from './Delivery';
+import PropTypes from 'prop-types';
+
+class ButtonList extends React.Component {
+
+
+
+    render() {
+        return this.props.buttons.map((btn) => {
+            if (btn.type === 'package') {
+                return (
+                    <p>
+                        <Package btn={btn} selectPackage={this.props.selectPackage}/>
+                    </p>
+                )
+            }
+
+            if (btn.type === 'delivery') {
+                return (
+                    <p>
+                        <Delivery btn={btn} selectDelivery={this.props.selectDelivery}/>
+                    </p>
+                )
+            }
+        });
+    }
+}
+
+ButtonList.propTypes = {
+    buttons: PropTypes.array.isRequired
+}
+
+export default ButtonList;
